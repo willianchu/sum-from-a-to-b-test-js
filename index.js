263fc59442d798cbb1f771c63e2e8c5f203fa4eb
willianchu <willianchu@hotmail.com>
2022-01-15 22:46:23 -0800

    implemented recursive sum

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 
+// eslint-disable-next-line func-style
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if (fromN > toN) {
+    return 0; //stop the recursion
+  }
+  let accumulator = fromN;
+  accumulator += sum(fromN + 1, toN); //recursive call
+  return accumulator;
 }
+
+
 
 module.exports = sum;
